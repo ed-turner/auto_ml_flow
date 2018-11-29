512be320317d2022f967c5f83debb637a6eba99b
Old Ed Turner1 <OldEdTurner1@Edwards-MacBook.home>
2018-11-29 08:56:14 -0500

    Finished the all the modules in this commit besides the ml_flow module

--- setup.py
+++ setup.py
@@ -12,14 +12,9 @@ REQUIREMENTS = ['pandas==0.23.4', 'numpy==1.15.4', 'sklearn==0.0', 'scipy==1.1.0
 
 setup(
     name='auto_ml_flow',
-    packages=['auto_ml_flow'],
+    packages=['auto_ml_flow', ],
     version=__version__,
     install_requires=REQUIREMENTS,
-    extras_require={
-        'complete':  ["xgboost", "lgbm"],
-        'xgb': ["xgboost"],
-        'lgbm': ['lgbm']
-    },
     author="Edward Turner",
     author_email="edward.turnerr@gmail.com",
     description=README,
